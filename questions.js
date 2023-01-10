96a7b03f4509b97022cb37ee0e3042f1afd2939a
Joshua <janorris65@gmail.com>
2023-01-10 11:32:26 -0600

    add inquirer questions for Add to table

--- questions.js
+++ questions.js
@@ -16,6 +16,55 @@ const questions = [
   },
 ];
 
+const addADepartment = [
+  {
+    type: "input",
+    name: "departmentName",
+    message: "What is the department's name",
+  },
+];
+
+const addAnEmployee = [
+  {
+    type: "input",
+    name: "employeeFirstName",
+    message: "What's the employee's First name",
+  },
+  {
+    type: "input",
+    name: "employeeLastName",
+    message: "What is the employee's Last name",
+  },
+  {
+    type: "input",
+    name: "employeeRole",
+    message: "What is the employee's role",
+  },
+  {
+    type: "input",
+    name: "employeeManager",
+    message: "What is their manager's name",
+  },
+];
+
+const addARole = [
+  {
+    type: "input",
+    name: "roleName",
+    message: "What is the role's name",
+  },
+  {
+    type: "input",
+    name: "roleSalary",
+    message: "What is the salary",
+  },
+  {
+    type: "input",
+    name: "roleDepartment",
+    message: "What is the department?",
+  },
+];
+
 module.exports = {
   One: questions,
 };

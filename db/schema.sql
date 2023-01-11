869a6e78aacdebed69805633b3737b88e924b2a1
Joshua <janorris65@gmail.com>
2023-01-10 23:05:07 -0600

    first attempt at tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,25 @@ CREATE DATABASE fakecompany1_2023_db;
 USE fakecompany1_2023_db;
 
 CREATE TABLE departments (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE employees (
-  id INT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id)
   REFERENCES role(id),
-  FOREIGN KEY (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id)
   REFERENCES employees(id),
+  ON DELETE SET NULL,
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2),
+  CONSTRAINT fk_department FOREIGN KEY (department_id)
+  REFERENCES department(id),
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,23 @@
-INSERT INTO students (first_name, last_name, enrolled)
-VALUES ("Elliot", "Smith", false),
-       ("Amira", "Afzal", true),
-       ("Christoper", "Lee", true),
-       ("Verónica", "Rodriguez", false),
-       ("Igor", "Stein", true);
-       
+INSERT INTO departments (name)
+VALUES ("Dept of Fun"),
+       ("Dept of Happy"),
+       ("Dept of Great Times"),
+       ("Dept of Long Walks"),
+       ("Dept of Songs");
+
+INSERT INTO employees (first_name,last_name)
+VALUES ("John","Smith"),
+       ("Jane","Smith"),
+       ("Dan","Smith"),
+       ("Bill","Spooty"),
+       ("Reginald","Heginald"),
+       ("Fats","Domino"),
+       ("Sugar","Bewild"),
+       ("Tony","Thepony"),
+       ("Tammy","Loami");
+
+INSERT INTO role (title, salary)
+VALUES ("Department Director", 10.00),
+       ("Assistant Director", 8.00),
+       ("Administrator", 6.00),
+       ("Foundling", 4.00);

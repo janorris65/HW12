f51104c46573dcc685dc4779987e53f6ac9aefeb
Joshua <janorris65@gmail.com>
2023-01-11 23:27:28 -0600

    create database and tables then seed in sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,11 @@
-DROP DATABASE IF EXISTS fakecompany1_2023_db;
-CREATE DATABASE fakecompany1_2023_db;
+DROP DATABASE IF EXISTS fakecompany_db;
+CREATE DATABASE fakecompany_db;
 
-USE fakecompany1_2023_db;
+USE fakecompany_db;
 
 CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-);
-
-CREATE TABLE employees (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  CONSTRAINT fk_role FOREIGN KEY (role_id)
-  REFERENCES role(id),
-  manager_id INT,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id)
-  REFERENCES employees(id)
-  ON DELETE SET NULL,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
@@ -27,5 +14,17 @@ CREATE TABLE role (
   salary DECIMAL(10,2),
   department_id INT,
   CONSTRAINT fk_department FOREIGN KEY (department_id)
-  REFERENCES department(id),
+  REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  CONSTRAINT fk_role FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  manager_id INT
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,9 +5,15 @@ VALUES (1,"Dept of Fun"),
        (4,"Dept of Long Walks"),
        (5,"Dept of Songs");
 
+INSERT INTO role (id,title, salary, department_id)
+VALUES (1,"Department Director", 10.00,1),
+       (2,"Assistant Director", 8.00,1),
+       (3,"Administrator", 6.00,1),
+       (4,"Foundling", 4.00,1);
+
 INSERT INTO employees (id,first_name,last_name, role_id, manager_id)
 VALUES (1,"John","Smith",2,2),
-       (2,"Jane","Smith",1,2),
+       (2,"Jane","Smith",1,NULL),
        (3,"Dan","Smith",2,2),
        (4,"Bill","Spooty",3,3),
        (5,"Reginald","Heginald",3,1),
@@ -15,9 +21,3 @@ VALUES (1,"John","Smith",2,2),
        (7,"Sugar","Bewild",4,6),
        (8,"Tony","Thepony",4,6),
        (9,"Tammy","Loami",4,6);
-
-INSERT INTO role (id,title, salary, department_id)
-VALUES (1,"Department Director", 10.00,1),
-       (2,"Assistant Director", 8.00,1),
-       (3,"Administrator", 6.00,1),
-       (4,"Foundling", 4.00,1);
